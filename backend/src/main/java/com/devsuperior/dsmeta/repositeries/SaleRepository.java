ea2d14345d722ceef24fc23f4d36aa0ea520df8b
unknown <syllasbraga2@gmail.com>
2022-07-23 20:34:12 -0300

    Api test

---
+++ backend/src/main/java/com/devsuperior/dsmeta/repositeries/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dsmeta.repositeries;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsmeta.entidades.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
